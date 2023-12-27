3d22be0431cd8a22b2581acc61f814b1e8f73ac8
Denish Chotaliya <128218250+DenishChotaliya@users.noreply.github.com>
2023-12-27 15:14:20 +0530

    TodoList use redux

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Addtodo from './compo/Addtodo';
+import Todos from './compo/Todos';
+
+function App() {
+  return (
+    <>
+    <h1 >Hello  Redux</h1>
+    <Addtodo/>
+    <Todos/>
+    </>
+  );
+}
+export default App;
---
+++ src/compo/Addtodo.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import {addTodo} from "../features/todo/todoSlice"
+import {useDispatch} from "react-redux";
+
+function Addtodo() {
+
+  const[input, setInput]= useState("")
+  const dispatch = useDispatch()
+
+  const addtodoHendal = (e)=>{
+    e.preventDefault()
+    dispatch(addTodo(input))
+    setInput("")
+  
+  }
+
+  
+
+  return (
+    <>
+      <form onSubmit={addtodoHendal} className="space-x-3 mt-12">
+       
+        <input type="text"
+        className="  bg-gray-800 rounded border border-gray-700 focus:border-indigo-500
+        focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        placeholder="Enter a Todo....."
+        value={input}
+        onChange={(get)=>{setInput(get.target.value)}}
+        />
+        <button
+        type="submit"
+        className="text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        >
+            Add Todo
+        </button>
+      </form>
+    </>
+  );
+}
+export default Addtodo;
